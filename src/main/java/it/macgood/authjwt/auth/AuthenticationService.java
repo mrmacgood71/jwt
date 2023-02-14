b8c9ed6d58d1ca3e3448fea7bad69ec1e0c1f77f
macgood <bo2002277@gmail.com>
2023-02-14 19:29:45 +0300

    init env variables

--- src/main/java/it/macgood/authjwt/auth/AuthenticationService.java
+++ src/main/java/it/macgood/authjwt/auth/AuthenticationService.java
@@ -25,6 +25,7 @@ public class AuthenticationService {
 
 
     public AuthenticationResponse register(RegisterRequest request) {
+
         var user = User.builder()
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
